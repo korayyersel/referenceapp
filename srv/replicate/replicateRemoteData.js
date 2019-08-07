fc9817f1a7d55755a9781e5f2476b33a72dc59ef
korayyersel <koray.yersel@q-perior.com>
2019-08-07 11:03:00 +0000

    move scheduler code to a library

--- srv/replicate/replicateRemoteData.js
+++ srv/replicate/replicateRemoteData.js
@@ -1,10 +1,8 @@
 "use strict";
 
-var https = require('https');
-var express = require("express");
 var hdbext = require("@sap/hdbext");
 var xsenv = require("@sap/xsenv");
-var jobsc = require("@sap/jobs-client");
+var schedulerLib = require("../lib/schedulerLib");
 
 module.exports = function (app) {
 	app.get("/replicate/replicateRemoteData", function (req, res) {
@@ -18,19 +16,8 @@ module.exports = function (app) {
 			scheduleId: jobScheduleId,
 			runId: jobRunId,
 			data: ''
-		};
-
-		var jobOptions = xsenv.getServices({
-			jobs: {
-				tag: "jobscheduler"
-			}
-		});
-		var schedulerOptions = {
-			baseURL: jobOptions.jobs.url,
-			token: '',
-			timeout: 15000
-		};
-
+		};     
+        
 		var promise = prepareProcedure();
 		promise
 			.then(function ([client, sp]) {
@@ -41,11 +28,11 @@ module.exports = function (app) {
 						if (err2) {
 							var errMsg2 = "DB execute procedure error: " + JSON.stringify(err2);
 							console.log(errMsg2);
-							updateJob(schedulerUpdateRequest, schedulerOptions, jobOptions, false, errMsg2);
+							schedulerLib.updateJob(schedulerUpdateRequest, false, errMsg2);
 							return;
 						}
 						console.log("replicateRemoteData job ended...");
-						updateJob(schedulerUpdateRequest, schedulerOptions, jobOptions, true, "job ended succesfully");
+						schedulerLib.updateJob(schedulerUpdateRequest, true, "job ended succesfully");
 						return;
 					});
 				});
@@ -88,81 +75,3 @@ function prepareProcedure() {
 		});
 	});
 };
-
-function getJobSchedulerAPIToken(jobOptions) {
-	return new Promise(function (resolve, reject) {
-		var https = require('https');
-		var querystring = require('querystring');
-
-		var host = jobOptions.jobs.uaa.url.toString().replace("https://", "").replace("http://", "");
-		var clientid = jobOptions.jobs.uaa.clientid;
-		var clientsecret = jobOptions.jobs.uaa.clientsecret;
-
-		// form data
-		var postData = querystring.stringify({
-			grant_type: "client_credentials",
-			client_id: clientid,
-			client_secret: clientsecret
-		});
-
-		// request option
-		var options = {
-			host: host,
-			port: 443,
-			method: 'POST',
-			path: '/oauth/token',
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded',
-				'Content-Length': postData.length
-			}
-		};
-
-		// request object
-		var req = https.request(options, function (res) {
-			var result = '';
-			res.on('data', function (chunk) {
-				result += chunk;
-			});
-			res.on('end', function () {
-				//console.log(result);
-				var resultJSON = JSON.parse(result);
-				if (resultJSON && resultJSON.access_token) {
-					resolve(resultJSON.access_token);
-				} else {
-					reject();
-				};
-			});
-			res.on('error', function (err) {
-				console.log(err);
-				reject();
-			})
-		});
-
-		// req error
-		req.on('error', function (err) {
-			console.log(err);
-		});
-
-		//send request witht the postData form
-		req.write(postData);
-		req.end();
-	});
-};
-
-function updateJob(schedulerUpdateRequest, schedulerOptions, jobOptions, success, message) {
-	var tokenPromise = getJobSchedulerAPIToken(jobOptions);
-	tokenPromise.then(function (token) {
-		var schedulerUpdateBody = {
-			success: success,
-			message: message
-		};
-		schedulerOptions.token = token;
-		var scheduler = new jobsc.Scheduler(schedulerOptions);
-		schedulerUpdateRequest.data = schedulerUpdateBody;
-		scheduler.updateJobRunLog(schedulerUpdateRequest, function (err, result) {
-			if (err) {
-				return console.log('Error updating run log: %s', err);
-			}
-		});
-	});
-}
